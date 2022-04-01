e90d05b5ca2a1582bd06791237360a3d0910d120
victorobrien <victorobrien@hotmail.com>
2022-04-01 11:48:33 -0300

    Fix uuid error

--- src/hooks/Toast.tsx
+++ src/hooks/Toast.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-use-before-define
 import React, { createContext, useContext, useCallback, useState } from 'react';
 
-import { uuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 
 import ToastContainer from '../components/ToastContainer';
 
@@ -22,21 +22,24 @@ const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 const ToastProvider: React.FC = ({ children }) => {
   const [messages, setMessages] = useState<ToastMessage[]>([]);
 
-  const addToast = useCallback(({ type, title, description }: Omit<ToastMessage, 'id'>) => {
-    const id = uuid();
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
 
-    const toast = {
-      id,
-      type,
-      title,
-      description,
-    };
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
 
-    setMessages(state => [...state, toast]);
-  }, []);
+      setMessages((state) => [...state, toast]);
+    },
+    []
+  );
 
   const removeToast = useCallback((id: string) => {
-    setMessages(state => state.filter(message => message.id !== id));
+    setMessages((state) => state.filter((message) => message.id !== id));
   }, []);
 
   return (
